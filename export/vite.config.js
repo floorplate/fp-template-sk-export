b65cf3f313d040af54a1aa2cf92ee59bff4697e6
chasbrouck <connerhasbrouck@gmail.com>
2026-03-16 11:35:38 -0500

    new vite config

--- export/vite.config.js
+++ export/vite.config.js
@@ -1,16 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import vue2 from "@vitejs/plugin-vue2";
 
 export default defineConfig({
     plugins: [
-        vue2(),
         laravel({
             input: [
                 "resources/css/site.css",
                 "resources/js/site.js",
-                "resources/js/cp.js",
             ],
             refresh: true,
         }),
